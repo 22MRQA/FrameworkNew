63f057f277eea6b576e9a2300de867b48def3965
Mykola Repetskyi <22MRQA@gmail.com>
2025-10-27 20:21:44 +0300

    upDateDriver

--- src/test/java/basesClass/TestInit.java
+++ src/test/java/basesClass/TestInit.java
@@ -5,6 +5,7 @@ package basesClass;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,6 +15,16 @@ public class TestInit {
 
     @BeforeMethod
     public void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 🧠 Додаємо аргументи, щоб браузер коректно працював у CI (GitHub Actions)
+        options.addArguments("--headless=new");            // запускає браузер без UI
+        options.addArguments("--no-sandbox");              // потрібне для Linux runner'а
+        options.addArguments("--disable-dev-shm-usage");   // уникає обмежень пам’яті
+        options.addArguments("--disable-gpu");             // вимикає GPU-рендеринг
+        options.addArguments("--window-size=1920,1080");   // фіксований розмір екрана
+
         WebDriver webDriver = new ChromeDriver();
         driver.set(webDriver);
     }
